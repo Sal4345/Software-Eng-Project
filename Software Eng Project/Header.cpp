466d649de3e61d84ae1c137ab2934359adc67df0
Salih <salaz540983@gmail.com>
2025-04-20 13:55:43 -0500

    Modified Main Program.cpp

--- Software Eng Project/Header.cpp
+++ Software Eng Project/Header.cpp
@@ -24,4 +24,4 @@ private:
 
 User::string getUsername() {
 
-}
+};
---
+++ Software Eng Project/Main Program.cpp
@@ -0,0 +1,9 @@
+#include "Header.h"
+#include <iostream>
+using namespace std;
+
+
+int main() {
+	cout << "Access Control Program V.10" << endl;
+
+}
