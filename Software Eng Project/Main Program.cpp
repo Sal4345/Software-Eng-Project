fe5d1d892373aeb675b9228fc1b5e61f387e3532
Salih <salaz540983@gmail.com>
2025-04-20 14:09:30 -0500

    Modified Main Program.cpp Added system output for main menu and comments explaining code

--- Software Eng Project/Main Program.cpp
+++ Software Eng Project/Main Program.cpp
@@ -4,6 +4,9 @@ using namespace std;
 
 
 int main() {
-	cout << "Access Control Program V.10" << endl;
-	cout << "Welcome! Please choose an option from the list: " << endl;
+	cout << "Access Control Program V.10" << endl;                                                          // Salih Azaz (4/20/2025)
+	cout << "Welcome! Please type the number of the option you wish to select: " << endl;                   // UI for Access Control Program
+	cout << "1. Registration\n" << "2. System Login\n" << "3. Access Modify (admin only)\n";                // Registration selection, System Login selection, Access Level Modify
+		                                                                                                    // (admin only) is WIP
+	
 }
