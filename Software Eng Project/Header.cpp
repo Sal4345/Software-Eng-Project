2c4deaabccbbb653f958015c84f895299933919d
Salih <salaz540983@gmail.com>
2025-04-20 13:52:48 -0500

    Added Header file (header.cpp) Added Main file (Main Program.cpp)

---
+++ Software Eng Project/Header.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class User
+{
+
+public:
+	string getUsername();
+	void setUsername();
+	string getUserID();
+	void setUserID();
+	string getUserAccessLevel();
+	void setUserAccessLevel();
+	bool isUserRegistered();
+
+private:
+	string username;
+	string UserID;
+	string UserAccessLevel;
+
+}
+
+
+User::string getUsername() {
+
+}
