d73b745ad9981998ef23faa3bf2615e8eb86d5a4
boomuh <144285923+boomuh@users.noreply.github.com>
2025-04-22 12:27:04 -0500

    I divided sections for better formating

--- Software Eng Project/Main Program.cpp
+++ Software Eng Project/Main Program.cpp
@@ -1,10 +1,27 @@
+// --- Standard Library Includes ---
+#include <iostream>  
+#include <vector>    
+#include <string>    
+#include <map>       
+#include <chrono>    
+#include <iomanip>   
+#include <ctime>     
+#include <limits>    
+#include <algorithm> 
+#include <sstream>   
+using namespace std;
+
+// --- Custom Header Includes ---
+// Include all the class definition header files
 #include "Header.h"
 #include "AccessLog.h"
 #include "AccessLogEntry.h"
-#include <iostream>
-using namespace std;
+#include "UserInterface.h"
+//#include "AccessControlSystem.h"
 
 
+
+//---------main-------------------------
 int main() {
 	cout << "Access Control Program V.10" << endl;                                                          // Salih Azaz (4/20/2025)
 	cout << "Welcome! Please type the number of the option you wish to select: " << endl;                   // UI for Access Control Program
